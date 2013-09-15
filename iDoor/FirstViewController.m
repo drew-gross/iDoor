2d2754cd35bcb64f614bf40149b85e2c03e39408
Drew Gross <drew.a.gross@gmail.com>
2013-09-14 23:09:04 -0400

    switch to sinatra

--- iDoor/FirstViewController.m
+++ iDoor/FirstViewController.m
@@ -30,7 +30,23 @@
 }
 
 - (IBAction)sendMessage:(id)sender {
-    UIAlertView *sentView = [[UIAlertView alloc] initWithTitle:@"Message Sent!" message:[@"Your message has been sent: " stringByAppendingString:self.messageField.text] delegate:nil cancelButtonTitle:@"Hooray!" otherButtonTitles:nil];
+    NSString *message = self.messageField.text;
+    
+    UIAlertView *sentView = [[UIAlertView alloc] initWithTitle:@"Message Sent!" message:[@"Your message has been sent: " stringByAppendingString:message] delegate:nil cancelButtonTitle:@"Hooray!" otherButtonTitles:nil];
     [sentView show];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[@"http://idoor.meteor.com/new_message/" stringByAppendingString:message]]
+                                                                           cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                                       timeoutInterval:10];
+    
+    [request setHTTPMethod: @"GET"];
+    
+    NSError *requestError;
+    NSURLResponse *urlResponse = nil;
+    
+    
+    NSData *response1 = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
+    [response1 description];
+    NSLog(@"%@", [[NSString alloc] initWithBytes:[response1 bytes] length:[response1 length] encoding:NSUTF8StringEncoding]);
 }
 @end
