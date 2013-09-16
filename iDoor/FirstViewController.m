291c3b29adbf1d6254db346560e8cb0e82add4b6
Drew Gross <drew.a.gross@gmail.com>
2013-09-16 18:14:23 -0400

    LRResty version working

--- iDoor/FirstViewController.m
+++ iDoor/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FirstViewController.h"
+#import <LRResty.h>
 
 @interface FirstViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *messageField;
@@ -32,21 +33,9 @@
 - (IBAction)sendMessage:(id)sender {
     NSString *message = self.messageField.text;
     
-    UIAlertView *sentView = [[UIAlertView alloc] initWithTitle:@"Message Sent!" message:[@"Your message has been sent: " stringByAppendingString:message] delegate:nil cancelButtonTitle:@"Hooray!" otherButtonTitles:nil];
-    [sentView show];
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[@"http://idoor.meteor.com/new_message/" stringByAppendingString:message]]
-                                                                           cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
-                                                                       timeoutInterval:10];
-    
-    [request setHTTPMethod: @"GET"];
-    
-    NSError *requestError;
-    NSURLResponse *urlResponse = nil;
-    
-    
-    NSData *response1 = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
-    [response1 description];
-    NSLog(@"%@", [[NSString alloc] initWithBytes:[response1 bytes] length:[response1 length] encoding:NSUTF8StringEncoding]);
+    [[LRResty client] post:@"http://idoor.herokuapp.com/messages" payload:@{@"content": message} withBlock:^(LRRestyResponse *response){
+        UIAlertView *sentView = [[UIAlertView alloc] initWithTitle:@"Message Sent!" message:@"Your message has been sent!" delegate:nil cancelButtonTitle:@"Hooray!" otherButtonTitles:nil];
+        [sentView show];
+    }];
 }
 @end
